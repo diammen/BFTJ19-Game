761a6338e29921249dde9677eafa5ba5b6cba0fb
Noah Albright <albright.noahk@gmail.com>
2019-10-16 17:56:08 -0700

    spawner base implementation

---
+++ Assets/NoahWorkspace/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] spawnPool;
+    public float spawnRate;
+
+    private float spawnTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer <= 0)
+        {
+            Instantiate(spawnPool[Random.Range(0, spawnPool.Length)], transform.position + (Random.insideUnitSphere * 0.2f), Random.rotation);
+            spawnTimer = spawnRate;
+        }
+        else
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+    }
+}
