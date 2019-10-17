aa68bd29b3a82537668ea433d5a2f812a255a74c
diammen <marcelnamdao@gmail.com>
2019-10-16 17:17:41 -0700

    remade project in 2018.3.8

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public TriggerCheck groundCheck;
+    public float jumpForce;
+    public float fallMult;
+    public float lowJumpMult;
+
+    Rigidbody rb;
+    bool doJump;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck.isColliding)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.AddForce(Vector3.up * Physics.gravity.y * (fallMult - 1) * Time.deltaTime, ForceMode.Impulse);
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * Physics.gravity.y * (lowJumpMult - 1) * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public bool isColliding;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (layerMask == (layerMask | 1 << other.gameObject.layer))
+        {
+            isColliding = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (layerMask == (layerMask | 1 << other.gameObject.layer))
+        {
+            isColliding = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isColliding = false;
+    }
+}
